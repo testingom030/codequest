47fea52bae5c47a7f633feead45168d3e78a180b
testingom30 <omchoksi99@gmail.com>
2025-06-09 15:12:40 +0530

    fix: Correct baseURL in API configuration for accurate backend endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,7 @@
-imp    baseURL: process.env.REACT_APP_API_URL || "https://backend-fsfowzhwc-aimls-projects-a90948ce.vercel.app",
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },xios from "axios";
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "https://backend-pqdzan1zs-aimls-projects-a90948ce.vercel.app",
+    baseURL: process.env.REACT_APP_API_URL || "https://backend-opgobbzkx-aimls-projects-a90948ce.vercel.app",
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
